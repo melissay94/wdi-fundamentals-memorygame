10637c3008a0b1b0240fe6dafefe288e4a30e32f
Melissa Young <melissay94@yahoo.com>
2020-01-13 15:43:53 -0800

    Logic added for alerts

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-let cardOne = "winnie";
-let cardTwo = "winnie";
-let cardThree = "disappointed";
-let cardFour = "disappointed";
+const cards = ["winnie", "winnie", "disappointed", "disappointed"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardOne = cards[0];
+let cardTwo = cards[2];
+let cardThree = cards[1];
+let cardFour = cards[3];
+
+const cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped winnie the pooh meme.");
+console.log("User flipped disappointed guy meme.");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
