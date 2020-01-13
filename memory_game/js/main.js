352c7938952d7b9d8bcf47b740add2119094b189
Melissa Young <melissay94@yahoo.com>
2020-01-13 12:24:01 -0800

    Started game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "winnie";
+let cardTwo = "winnie";
+let cardThree = "disappointed";
+let cardFour = "disappointed";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
