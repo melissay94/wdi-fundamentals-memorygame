ab569909a134260adddbdb3d58caf44924e20368
Melissa Young <melissay94@yahoo.com>
2020-01-13 19:44:48 -0800

    Broke logic up into functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,26 @@
 console.log("Up and running!");
 
 const cards = ["winnie", "winnie", "disappointed", "disappointed"];
-
-let cardOne = cards[0];
-let cardTwo = cards[2];
-let cardThree = cards[1];
-let cardFour = cards[3];
-
 const cardsInPlay = [];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped winnie the pooh meme.");
-console.log("User flipped disappointed guy meme.");
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
+function checkForMatch() {
+    
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Sorry, try again.");
+        }
     }
 }
+
+function flipCard(cardId) {
+    
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
