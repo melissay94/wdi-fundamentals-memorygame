1e05fd5396f5d3608ad8e929168929678a2ee61e
Melissa Young <melissay94@yahoo.com>
2020-01-13 20:28:58 -0800

    Switched string rep of cards to objects of cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
 console.log("Up and running!");
 
-const cards = ["winnie", "winnie", "disappointed", "disappointed"];
+const cards = [];
 const cardsInPlay = [];
+
+function createPlayingCard(name, panel, cardImage) {
+    let card = {
+        name: name,
+        panel: panel, 
+        cardImage: cardImage
+    }
+
+    cards.push(card);
+}
+
+createPlayingCard("winnie", 1, "images/meme_2_first_panel.jpg");
+createPlayingCard("winnie", 2, "images/meme_2_second_panel.jpg");
+createPlayingCard("disappointed", 1, "images/meme_1_first_panel.jpg");
+createPlayingCard("disappointed", 2, "images/meme_1_second_panel.jpg");
 
 function checkForMatch() {
     
@@ -16,8 +31,11 @@ function checkForMatch() {
 
 function flipCard(cardId) {
     
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].name);
+    cardsInPlay.push(cards[cardId].name);
+
+    console.log("Card Image: " + cards[cardId].cardImage);
+    console.log("Panel Number: " + cards[cardId].panel);
 
     checkForMatch();
 }
